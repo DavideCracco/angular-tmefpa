196db19857699b1ab67c74e7bd4aaad5bb9a7d14
DavideCracco <davidecracco95@gmail.com>
2020-01-02 15:37:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tmefpa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tmefpa)
---
+++ src/app/controller/controller.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-controller',
+  templateUrl: './controller.component.html',
+  styleUrls: ['./controller.component.css']
+})
+export class ControllerComponent implements OnInit {
+  element;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+    
+  }
+
+}
