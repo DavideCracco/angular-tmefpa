aa70950a1a02a2c991e42b824831621d18f855fd
DavideCracco <davidecracco95@gmail.com>
2020-01-03 18:04:18 +0100

    3 files modified

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -19,11 +19,6 @@ export class ListaComponent implements OnInit {
   }
 
   onTxtClick(element){
-    //console.log("lista -> " , this.lista);
-    console.log("element -> " , element);
-    //console.log("click on " + element);
-    //console.log("~>" , this.lista.indexOf(element));
-    //console.log("indexOf -> " , this.lista.indexOf(element));
     this.selectionEvent.emit({
       selection: this.lista.indexOf(element)
     });
