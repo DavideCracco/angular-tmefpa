f8caec3f98c7a70ca2bbeb75503aa5b13a10984c
DavideCracco <davidecracco95@gmail.com>
2020-01-03 10:39:36 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { ControllerComponent } from './controller/controller.component';
 import { ListaComponent } from './lista/lista.component';
 import { BusService } from './bus.service';
 import { ElementComponent } from './element/element.component';
+import { DisplayElementComponent } from './display-element/display-element.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ControllerComponent, ListaComponent, ElementComponent],
+  declarations: [ AppComponent, HelloComponent, ControllerComponent, ListaComponent, ElementComponent, DisplayElementComponent],
   bootstrap:    [ AppComponent ],
   providers: [BusService]
 })
---
+++ src/app/display-element/display-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-element',
+  templateUrl: './display-element.component.html',
+  styleUrls: ['./display-element.component.css']
+})
+export class DisplayElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
